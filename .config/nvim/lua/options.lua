bb042e0d41ffee6a7fb9d38c1a6fc21b589b9978
henrylewis2004 <henry@lewis-lewis.co.uk>
2026-03-04 03:15:20 +0000

    options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,6 +6,13 @@ vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
 
+vim.opt.cursorline = false
+vim.opt.smoothscroll = true
+
 -- mouse
 vim.opt.mouse=""
+
+vim.diagnostic.config({
+	signs=false,
+})
 
