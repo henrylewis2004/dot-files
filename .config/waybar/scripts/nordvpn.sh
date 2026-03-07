1ac9314befbd374ae9f8b913cb412c90efa32558
henrylewis2004 <henry@lewis-lewis.co.uk>
2026-03-07 17:27:29 +0000

    Nordvpn

--- .config/waybar/scripts/nordvpn.sh
+++ .config/waybar/scripts/nordvpn.sh
@@ -4,7 +4,7 @@ STATUS=$(nordvpn status | grep -i "Status:" | awk '{print $2}')
 COUNTRY=$(nordvpn status | grep -i "Country:" | awk '{$1=""; print $0}' | xargs)
 
 if [ "$STATUS" = "Connected" ]; then
-	echo "{\"text\": \"NordVPN 󰦝 \", \"tooltip\": \"NordVPN: Connected to $COUNTRY\", \"class\": \"connected\"}"
+	echo "{\"text\": \"󰦝 \", \"tooltip\": \"NordVPN: Connected to $COUNTRY\", \"class\": \"connected\"}"
 else
-	echo "{\"text\": \"NordVPN 󰦞 \", \"tooltip\": \"NordVPN: Disconnected\", \"class\": \"disconnected\"}"
+	echo "{\"text\": \"󰦞 \", \"tooltip\": \"NordVPN: Disconnected\", \"class\": \"disconnected\"}"
 fi
